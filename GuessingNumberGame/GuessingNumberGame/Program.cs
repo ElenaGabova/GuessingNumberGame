ac7c19fc6940c2fe3f51c1dc1e8c1440d1dba7de
ElenaGabova <Eleyurgab@gmail.com>
2022-02-19 23:18:45 +0300

    Добавила раскрашивание цветом консоли

--- GuessingNumberGame/GuessingNumberGame/Program.cs
+++ GuessingNumberGame/GuessingNumberGame/Program.cs
@@ -11,17 +11,19 @@ bool isNameCorrect = false;
 // Считываем имя пользователя, пока оно не будет корректным (имя не должно быть пустым, имя не должно содержать числа). 
 while(!isNameCorrect)
 {
+    Console.ForegroundColor = ConsoleColor.White;
     userNameInput = Console.ReadLine();
     if ((userNameInput.Length == 0) || (userNameInput.Any(c => char.IsNumber(c))))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine(Resources.NameIsNotCorrectMessage);
+    }
     else
         isNameCorrect = true;
 }
 
 userNameInput = userNameInput.Trim();
 userNameInput = userNameInput.Replace("  ", " ");
-
-Console.WriteLine();
 string? userNumberInput = null;
 string? messageIsNotCorrect = "";
 bool isWin = false;
@@ -36,14 +38,18 @@ int attemptCount = 1;
 //Пока не угадали число
 while(!isWin)
 {
-    bool isNumberCorrect = false;
+    bool isNumberCorrect = false;    
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("\n" +string.Format(Resources.NumberQuestionMessage, userNameInput, minNumber, maxNumber, attemptCount));
 
     // Считываем ввод числа пользователя, пока он не будет корректным.
     // (строка не должна быть пустой, строка должна содержать числа, числа должны попадать в диапозон minNumber...maxNumber)
     while (!isNumberCorrect)
     {
+        Console.ForegroundColor = ConsoleColor.White;
         userNumberInput = Console.ReadLine();
+        Console.ForegroundColor = ConsoleColor.Red;
+
         messageIsNotCorrect = string.Format(Resources.NumberNotCorrectMessage, userNumberInput, minNumber, maxNumber);
         if ((userNumberInput.Length == 0) || (!int.TryParse(userNumberInput, out userNumber)))
             Console.WriteLine(messageIsNotCorrect);
@@ -56,16 +62,24 @@ while(!isWin)
 
     //Сравниваем ввод пользователя с загаданным числом
     if (userNumber > secretNumber)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;       
         Console.WriteLine(string.Format(Resources.YourNumberMoreThanSecretMessage, userNumber));
+    }
+
     if (userNumber < secretNumber)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine(string.Format(Resources.YourNumberLessThanSecretMessage, userNumber));
+    }
     if (userNumber == secretNumber)
     {
+        Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine(string.Format(Resources.YourWinMessage, userNumber));
         isWin = true;
     }
 
+    Console.ForegroundColor = ConsoleColor.White;
     attemptCount += 1;
 }
-
-   
+Console.ReadLine();
